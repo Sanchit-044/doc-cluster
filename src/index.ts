950837a4c0921ba45783262f3cc8c67658d1c4ac
Sanchit Bhardwaj <sanchitbhardwaj044@gmail.com>
2025-12-14 17:04:46 +0530

    allowed localhost in cors

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const app: Application = express();
 //middlewares and cors settings
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.BASE_URL, process.env.ADMIN_URL],
+    origin: [process.env.FRONTEND_URL, process.env.BASE_URL, process.env.ADMIN_URL, "http://localhost:3000"],
     credentials: true,
   })
 );
